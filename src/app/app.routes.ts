2bfb4d1d4793604339dfae2fbe004b754a54c79b
John Magdy <j.magdy18@yahoo.com>
2026-04-16 18:34:50 +0200

    initialize components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { CustomerDetails } from './customer-details/customer-details';
+import { Transactions } from './transactions/transactions';
+import { CreateTransaction } from './create-transaction/create-transaction';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'customer/:id', component: CustomerDetails },
+  { path: 'transactions', component: Transactions },
+  { path: 'transactions/new', component: CreateTransaction },
+  { path: '**', redirectTo: '/login' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/create-transaction/create-transaction.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-transaction',
+  imports: [],
+  templateUrl: './create-transaction.html',
+  styleUrl: './create-transaction.css',
+})
+export class CreateTransaction {}
