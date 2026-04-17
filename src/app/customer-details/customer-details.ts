d5afcd50ddd3b495166332d9e24931b27ad73dd0
John Magdy <j.magdy18@yahoo.com>
2026-04-17 18:16:32 +0200

    table components clean up

--- src/app/customer-details/customer-details.ts
+++ src/app/customer-details/customer-details.ts
@@ -7,10 +7,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CapitalizePipe } from '../common/pipes/capitalize-pipe';
 import { IAccount } from '../interfaces/accountInterface';
 import { AccountService } from '../services/account/account-service';
+import { Table } from '../table/table';
 
 @Component({
   selector: 'app-customer-details',
-  imports: [CommonModule, CapitalizePipe],
+  imports: [CommonModule, CapitalizePipe, Table],
   templateUrl: './customer-details.html',
   styleUrl: './customer-details.css',
 })
@@ -21,12 +22,15 @@ export class CustomerDetails implements OnInit {
   router = inject(Router);
 
   customerData$: Observable<ICustomer | undefined> | undefined;
-  accountData$: Observable<IAccount[] | undefined> | undefined;
+  accountsData$: Observable<IAccount[] | undefined> | undefined;
+
+  accountsTableCustomeHeaders = ['Account Number', 'Type', 'Balance', 'Status'];
+  accountsTableHeaders = ['id', 'type', 'balance', 'status'];
 
   ngOnInit(): void {
     const customerID = this.route.snapshot.paramMap.get('id') || '';
     this.customerData$ = this.customerService.getCustomer(customerID);
-    this.accountData$ = this.accountService.getCustomerAccounts(customerID);
+    this.accountsData$ = this.accountService.getCustomerAccounts(customerID);
   }
 
   goBack() {
--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,15 +1,18 @@
 import { Component, input, output } from '@angular/core';
 import { CapitalizePipe } from '../common/pipes/capitalize-pipe';
+import { CurrencyPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-table',
-  imports: [CapitalizePipe],
+  imports: [CapitalizePipe, CurrencyPipe, NgClass],
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
 export class Table {
   headers = input.required<string[]>();
+  customeHeaders = input<string[]>();
   data = input.required<any[]>();
+  hasAction = input.required<boolean>();
   viewDetails = output<any>();
 
   onViewDetails(item: any) {
